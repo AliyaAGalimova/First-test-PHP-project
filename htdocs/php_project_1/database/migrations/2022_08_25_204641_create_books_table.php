17a4bf714f534f4a5449a0209f7c147daa8a9e04
AliyaAGalimova <aliya.galimova825@gmail.com>
2022-08-26 07:20:56 +0300

    3

--- htdocs/php_project_1/database/migrations/2022_08_25_204641_create_books_table.php
+++ htdocs/php_project_1/database/migrations/2022_08_25_204641_create_books_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("author_id");
+            $table->foreign("author_id")->references('id')->on('authors');
+            $table->text("name_book");
+            // $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('books');
     }
 };
---
+++ htdocs/php_project_1/resources/views/list_books.blade.php
@@ -0,0 +1,46 @@
+
+<html>
+    <body>
+        <h1>list_books</h1>
+        <table>
+            <tr>
+                <td>
+                    id1
+                </td>
+                <td>
+                    ФИО_Автора1
+                </td>
+                <td>
+                    Книга1
+                </td>
+                <td>
+                    X
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    id2
+                </td>
+                <td>
+                    ФИО_Автора2
+                </td>
+                <td>
+                    Книга2
+                </td>
+                <td>
+                    X
+                </td>
+            </tr>
+        </table>
+        <form method="post">
+            <label>Введите ФИО автора: <select name="name_book">
+                <option>Автор1</option>
+                <option>Автор2</option>
+                <option>Автор3</option>
+            </select>
+            </label><br />
+            <label>Введите название книги: <input name="name"></label><br />
+            <input type="submit"><br />
+        </form>
+    </body>
+</html>
